a3179660d96b1daef769fe07de36cd92f35265e6
Bachry28 <muhbahryalhadi@gmail.com>
2023-10-09 22:09:19 +0700

    change code query users table

--- router/users.js
+++ router/users.js
@@ -4,6 +4,22 @@ const pool = require('../database.js');
 const jwt = require('jsonwebtoken');
 const JWT_SECRET = "f4973e1e-a81b-415e-b9c0-70d95d5b7481"
 
+
+userRouter.use((req, res, next) => {  
+
+  const token = req.header('jwt-token')
+
+
+  try {
+    jwt.verify(token, JWT_SECRET)
+    next(); 
+  } catch (error) {
+    res.status(401).json({
+      description: 'Token tidak valid',
+    });
+  }
+});
+
 userRouter.post('/register', (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
@@ -42,4 +58,43 @@ userRouter.post('/register', (req, res) => {
     });
   });
   
+
+  userRouter.get('/', (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const offset = (page - 1) * limit;
+  
+    pool.query(
+      `SELECT COUNT(*) FROM users`, 
+      (err, countResult) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json(err);
+        }
+  
+        const totalCount = parseInt(countResult.rows[0].count);
+        const totalPages = Math.ceil(totalCount / limit);
+  
+        pool.query(
+          `SELECT * FROM users LIMIT $1 OFFSET $2`,
+          [limit, offset],
+          (err, result) => {
+            if (err) {
+              console.error(err);
+              return res.status(500).json(err);
+            }
+  
+            res.json({
+              data: result.rows,
+              totalItems: totalCount,
+              totalPages: totalPages,
+              currentPage: page,
+            });
+          }
+        );
+      }
+    );
+  });
+  
+
 module.exports = userRouter;
